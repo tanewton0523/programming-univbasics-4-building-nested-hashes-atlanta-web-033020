f95380f602c6f902544f3fe560789fc4cba921a8
TeMecha Newton <tanewton0523@gmail.com>
2020-03-17 01:51:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,9 +7,9 @@ end
 def monopoly_with_second_tier
   {
  railroads: {
-   :pieces => "4",
-   :rent_in_dollars => {},
-   :names => {}
+   pieces: "4",
+   rent_in_dollars: {},
+   names: {}
   }
  }
 
