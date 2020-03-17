4ac54129ee4d96c3ea00098396538676f7f53dfd
TeMecha Newton <tanewton0523@gmail.com>
2020-03-17 01:57:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,10 +25,10 @@ def monopoly_with_third_tier
      four_pieces_owned: 200,
    },
    names: {
-     reading_railroad => {}
-     pennsylvania_railroad => {}
-     b_and_o_railroad => {}
-     shortline_railroad => {}
+     reading_railroad => {},
+     pennsylvania_railroad => {},
+     b_and_o_railroad => {},
+     shortline_railroad => {},
    }
   }
  }
